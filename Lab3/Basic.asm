7a29fed3a3b06474e1c0112267a60e9341930687
Yoyeehaha <139629979+Yoyeehaha@users.noreply.github.com>
2024-09-29 21:10:25 +0800

    Update Basic.asm

--- Lab3/Basic.asm
+++ Lab3/Basic.asm
@@ -1,1 +1,21 @@
-
+List p=18f4520 
+    #include<p18f4520.inc>
+    CONFIG OSC = INTIO67
+    CONFIG WDT = OFF
+    org 0x00
+    
+    MOVLW b'01011111' ;input
+    CLRF TRISA
+    MOVWF TRISA
+    
+    ;left rotate
+    BCF STATUS, C
+    RLCF TRISA, 1
+    
+    ;right rotate
+    RRCF TRISA, 1
+    BTFSC TRISA, 6 ;if bit 6 is 0, skip next line
+    BSF TRISA, 7 ; set bit 7 to 1
+    BTFSS TRISA, 6 ;if bit 6 is 1, skip next line
+    BCF TRISA, 7 ; set bit 7 to 0
+    end
